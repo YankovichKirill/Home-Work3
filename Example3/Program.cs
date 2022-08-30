7727f2e1368ab69b85a11b27b7769b8bdc171b2c
YankovichKirill <kirill.best97@gmail.com>
2022-08-30 20:23:36 +0300

    Save3

---
+++ Example3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Enter the number: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+    int counter = 0;
+    int lenght = cube.Length;
+    while (counter < lenght){
+        cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+        counter++;
+    }
+}
+
+void PrintArry(int[] coll){
+    int count = coll.Length;
+    int index = 0;
+    while(index < count){
+        Console.Write(coll[index] + " ");
+        index++;
+    }
+}
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);

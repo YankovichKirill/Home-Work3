1ee303fd27edad36b1d9f21b86718a6465d8de15
YankovichKirill <kirill.best97@gmail.com>
2022-08-30 19:52:14 +0300

    Save1

---
+++ Example1/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter the number: ");
+        string? number = Console.ReadLine();
+
+        void CheckingNumber(string number){
+            if (number[0] == number[4] || number[1] == number[3]){
+                Console.WriteLine($"Your number: {number} - palindrome");
+            }
+            else Console.WriteLine($"Your number {number} - not a palindrome");
+        }
+        if (number!.Length == 5){
+            CheckingNumber(number);
+        }
+        else Console.WriteLine($"Enter the correct number");
+    }
+
+}
